df99eb148a8af239afc8b77bd707efe2037ff6b6
Aburafik <rfkabubakari@gmail.com>
2020-07-18 03:45:21 -0700

    update

--- lib/display_screen.dart
+++ lib/display_screen.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:simpleweather/constants.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class DisplayScreen extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class DisplayScreen extends StatefulWidget {
 
 class _DisplayScreenState extends State<DisplayScreen> {
   TextEditingController t1 = TextEditingController();
+  bool isLoading = false;
   var name;
   var search;
   var temp;
@@ -37,100 +39,107 @@ class _DisplayScreenState extends State<DisplayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image: AssetImage('images/res.jpg'),
+      body: ModalProgressHUD(
+        inAsyncCall: isLoading,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('images/res.jpg'),
+                ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Column(
-                  children: <Widget>[
-                    Text('WEATHER', style: TitleTextStyle),
-                    TextField(
-                      controller: t1,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20),
-                      decoration: InputDecoration(
-                          suffixIcon: IconButton(
-                              icon: Icon(
-                                Icons.clear,
-                                color: Colors.blue,
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  t1.text = '';
-                                  place = null;
-                                  temp = null;
-                                  description = null;
-                                });
-                              }),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.blue,
-                          ),
-                          hintText: 'Search any City',
-                          border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(10)),
-                          fillColor: Colors.white,
-                          filled: true),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      place != null ? place : 'cityName',
-                      style: CityNameTexStyle,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(temp != null ? temp.toString() + '\u00b0' : 'Temp',
-                        style: TemperatureTextStyle),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      description != null
-                          ? description.toString()
-                          : 'Description',
-                      style: TextStyle(
-                        fontSize: 20,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Text('WEATHER', style: TitleTextStyle),
+                      TextField(
+                        controller: t1,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20),
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: Colors.blue,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    t1.text = '';
+                                    place = null;
+                                    temp = null;
+                                    description = null;
+                                  });
+                                }),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.blue,
+                            ),
+                            hintText: 'Search any City',
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(10)),
+                            fillColor: Colors.white,
+                            filled: true),
                       ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Container(
-                      height: 40,
-                      width: 200,
-                      child: RaisedButton(
-                          color: Colors.blue,
-                          shape: StadiumBorder(),
-                          child: Text(
-                            'Search',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                          onPressed: () {
-                            setState(() {
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        place != null ? place : 'cityName',
+                        style: CityNameTexStyle,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(temp != null ? temp.toString() + '\u00b0' : 'Temp',
+                          style: TemperatureTextStyle),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        description != null
+                            ? description.toString()
+                            : 'Description',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Container(
+                        height: 40,
+                        width: 200,
+                        child: RaisedButton(
+                            color: Colors.blue,
+                            shape: StadiumBorder(),
+                            child: Text(
+                              'Search',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                isLoading = false;
+                              });
                               name = t1.text;
                               getData();
-                            });
-                          }),
-                    )
-                  ],
+                              setState(() {
+                                isLoading = true;
+                              });
+                            }),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
